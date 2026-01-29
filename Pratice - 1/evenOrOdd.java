b85dc4c21762e61a7280eb7b6a3d7f8b829eff2e
Mani <146062828+MANI-301@users.noreply.github.com>
2026-01-29 21:19:25 +0530

    Add files via upload

---
+++ Pratice - 1/evenOrOdd.java
@@ -0,0 +1,28 @@
+class  defaultValues
+{
+public static void main(String[] args) 
+	{
+	   byte a;
+	   short b;
+	   int c;
+	   long d;
+	   float e;
+	   double f;
+	   char g;
+	   boolean h;
+	   String i;
+		
+
+	
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println(f);
+			System.out.println(g);
+			System.out.println(h);
+			System.out.println(i);
+		
+	}
+}
---
+++ Pratice - 1/m.java
@@ -0,0 +1,11 @@
+class m 
+{
+	public static void main(String[] args) 
+	{
+		
+		char a ='a';
+		char b = 'b';
+		 int c =(int) a - b;
+		System.out.println(c);
+	}
+}
