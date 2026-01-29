583f33a7701918c4764594cdaf93e38891ad35cf
Mani <146062828+MANI-301@users.noreply.github.com>
2026-01-29 20:32:40 +0530

    Add files via upload

---
+++ Collection/C1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class C1 
+{
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		do{
+		System.out.print("Enter Num:");
+		int Num = sc.nextInt();
+		i++;
+		while (i<6)
+		{
+		ArrayList<Integer> l1 = new ArrayList();
+		l1.add(Num);
+		System.out.println(l1);
+		}
+	}}
+}
